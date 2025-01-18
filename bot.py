1eb4a933f0820056774a36789b0aa57590a79d52
Алина Чернявская <aiponalinachernavskaa@gmail.com>
2025-01-18 13:47:55 +0600

    Конечный код бота

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from telegram import (
     Update,
     ReplyKeyboardMarkup,
-    InlineKeyboardMarkup,
     InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
     ReplyKeyboardRemove,
 )
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
-import random, requests 
-from bs4 import BeautifulSoup
+import random, os
 
 CANDIDATES_INFO = """
 1. Чернявская Алина Игоревна
@@ -56,56 +56,51 @@ ZADACHI_INFO = """13.01.25-19.01.25:
 [Ссылка на таблицу](https://docs.google.com/spreadsheets/d/1sDnJk6gU4I1cbCagWUNymxGUQKUv7xy52OWJzTLkyyQ/edit#gid=0).
 """
 
-FACTS = [
-    "Улыбка - ваш лучший инструмент для команды!",
-    "Помните, что хороший план важнее хаотичной импровизации.",
-    "Чай с коллегой порой важнее миллионного отчета.",
-]
-
 EVENTS = [
     "18.01.25 - Ночная репетиция, время: 22:00",
     "25.01.25 - Ночная репетиция, время: 22:00",
     "01.02.25 - Ночная репетиция, время: 22:00",
 ]
 
-SIGN_TRANSLATION = {
-    "овен": "aries",
-    "телец": "taurus",
-    "близнецы": "gemini",
-    "рак": "cancer",
-    "лев": "leo",
-    "дева": "virgo",
-    "весы": "libra",
-    "скорпион": "scorpio",
-    "стрелец": "sagittarius",
-    "козерог": "capricorn",
-    "водолей": "aquarius",
-    "рыбы": "pisces"
-}
+MEMS = [
+    "https://i.pinimg.com/736x/a1/8b/06/a18b063a4a1bfdd5fb53afddb3616043.jpg",
+    "https://i.pinimg.com/736x/01/5c/16/015c16993ae37a337e212c931fe90ca8.jpg",
+    "https://i.pinimg.com/736x/6b/bf/22/6bbf22ae7a4f4ca9d4115b64e2b2e22d.jpg",
+    "https://i.pinimg.com/736x/b3/8e/69/b38e69bd410e31b0f3c2f06e6e510307.jpg",
+    "https://i.pinimg.com/736x/a5/a9/d9/a5a9d91e35e262242027fe07b991031c.jpg",
+    "https://i.pinimg.com/736x/72/fc/ed/72fced4b72d020e7a371095ce4209698.jpg",
+    "https://i.pinimg.com/736x/84/72/05/847205e7fbb4dc8a6bd396e7bdc80847.jpg",
+    "https://i.pinimg.com/736x/13/48/b6/1348b6d619cb54043813821577f15b2e.jpg",
+    "https://i.pinimg.com/736x/5b/b7/2e/5bb72ec359a6ecc7811f80124ce7d635.jpg",
+    "https://i.pinimg.com/736x/f8/4d/f4/f84df46aa202e2a40356b33fc3ac7847.jpg",
+]
 
 TELEGRAM_USER = "1493688059"
-EDIT_MODE = None  # Глобальная переменная для определения текущего режима редактирования
+TELEGRAM_TOKEN = "7619104440:AAHdFpaYtoCS6pE3B9Ii8WJjU0KaR4YxVFA"
+EDIT_MODE = None 
+URL = "https://"
+
+async def set_webhook() -> None:
+    # Укажите URL вашего бота
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    success = await application.bot.set_webhook(URL)
+    if success:
+        print("Webhook установлен успешно!")
+    else:
+        print("Ошибка при установке вебхука.")
 
 # Функция для создания основного меню
 def get_main_menu():
     keyboard = [
         ["Список кандидатов", "Контакты", "Ваши предложения"],  
         ["Задачи на неделю от командного состава"],
-        ["Календарь мероприятий", "Гороскоп дня"],  
+        ["Календарь мероприятий", "Жизненый или милый мем"],  
     ]
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 # Функция для создания inline-кнопок
 def get_return_button():
     keyboard = [[InlineKeyboardButton("Возврат к меню", callback_data="return_to_menu")]]
-    return InlineKeyboardMarkup(keyboard)
-
-def get_horoscope_menu():
-    zodiac_signs = [
-        "Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева", 
-        "Весы", "Скорпион", "Стрелец", "Козерог", "Водолей", "Рыбы"
-    ]
-    keyboard = [[InlineKeyboardButton(sign, callback_data=f"horoscope_{sign}")] for sign in zodiac_signs]
     return InlineKeyboardMarkup(keyboard)
 
 # Обработчик команды /start
@@ -134,15 +129,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     if user_message == "Список кандидатов":
         await update.message.reply_text(f"Список кандидатов:\n{CANDIDATES_INFO}")
     elif user_message == "Контакты":
-        await update.message.reply_text("Связь с нами: aboba228@rambler.com")
+        await update.message.reply_text("Группа ВК: https://vk.com/osd_sibhar \nТелеграмм канал: https://t.me/osd_sibhar \nГлавный разработчик(ТГ аккаунт): @FistQunami")
     elif user_message == "Задачи на неделю от командного состава":
         await update.message.reply_text(f"Задачи на неделю:\n{ZADACHI_INFO}", disable_web_page_preview=True)
     elif user_message == "Ваши предложения":
         await handle_suggestions_start(update, context)
     elif user_message == "Календарь мероприятий":
         await calendar(update, context)
-    elif user_message == "Гороскоп дня":
-        await horoscope(update, context)
+    elif user_message == "Жизненый или милый мем":
+        await meme_of_the_day(update, context)
     else:
         await update.message.reply_text("Пожалуйста, выберите команду из меню.", reply_markup=get_main_menu())
 
@@ -187,26 +182,39 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         )
         context.user_data["waiting_for_suggestions"] = False
 
+async def meme_of_the_day(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправить случайный мем."""
+    if MEMS:
+        meme_url = random.choice(MEMS)
+        await update.message.reply_photo(photo=meme_url)
+    else:
+        await update.message.reply_text("Извините, сейчас мемов нет.")
+
 # Режим редактирования для админа
 async def handle_admin_edit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global EDIT_MODE, FACTS, EVENTS
+    global EDIT_MODE, EVENTS, ZADACHI_INFO, MEMS
 
     new_text = update.message.text
 
-    if EDIT_MODE == "FACTS":
-        FACTS.append(new_text)
-        await update.message.reply_text("Цитата добавлена!", reply_markup=get_main_menu())
-    elif EDIT_MODE == "EVENTS":
+    if EDIT_MODE == "EVENTS":
         EVENTS.append(new_text)
         await update.message.reply_text("Событие добавлено в календарь!", reply_markup=get_main_menu())
+    elif EDIT_MODE == "TASKS":
+        ZADACHI_INFO = new_text
+        await update.message.reply_text("Задачи недели обновлены!", reply_markup=get_main_menu())
+    elif EDIT_MODE == "ADD_MEME":
+        new_meme = update.message.text
+        MEMS.append(new_meme)
+        await update.message.reply_text("Мем добавлен!", reply_markup=get_main_menu())
+        EDIT_MODE = None
 
     EDIT_MODE = None
 
-# Обработчик команды /edit_facts
-async def edit_facts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+# Обработчик команды /edit_tasks
+async def edit_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global EDIT_MODE
-    EDIT_MODE = "FACTS"
-    await update.message.reply_text("Введите новую цитату дня:", reply_markup=ReplyKeyboardRemove())
+    EDIT_MODE = "TASKS"
+    await update.message.reply_text("Введите новые задачи на неделю:", reply_markup=ReplyKeyboardRemove())
 
 # Обработчик команды /edit_calendar
 async def edit_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -214,83 +222,41 @@ async def edit_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     EDIT_MODE = "EVENTS"
     await update.message.reply_text("Введите новое событие для календаря:", reply_markup=ReplyKeyboardRemove())
 
+async def add_meme(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Добавить новый мем в список (только для админа)."""
+    global EDIT_MODE
+    if str(update.effective_user.id) == TELEGRAM_USER:
+        EDIT_MODE = "ADD_MEME"
+        await update.message.reply_text("Отправьте URL нового мема:", reply_markup=ReplyKeyboardRemove())
+    else:
+        await update.message.reply_text("У вас нет прав для выполнения этой команды.")
+
 # Календарь событий
 async def calendar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = "\n".join(EVENTS)
     await update.message.reply_text(f"Календарь мероприятий:\n{message}")
-
-async def horoscope(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        "Выберите ваш знак зодиака:",
-        reply_markup=get_horoscope_menu()
-    )
-
-async def handle_horoscope_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    await query.answer()
-
-    selected_sign = query.data.replace("horoscope_", "").lower()
-    translated_sign = SIGN_TRANSLATION.get(selected_sign)
-
-    if not translated_sign:
-        await query.message.reply_text(
-            f"Не удалось найти гороскоп для знака {selected_sign}.",
-            reply_markup=get_main_menu(),
-        )
-        return
-
-    horoscope_url = f"https://horo.mail.ru/prediction/{translated_sign}/today/"
-
-    try:
-        response = requests.get(horoscope_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # Найти текст гороскопа
-        horoscope_block = soup.find("div", class_="article__item article__item_alignment_left article__item_html")
-        horoscope_text = horoscope_block.text.strip() if horoscope_block else "Гороскоп не найден."
-
-        await query.message.reply_text(
-            f"Гороскоп для знака {selected_sign}:\n\n{horoscope_text}",
-            reply_markup=get_main_menu(),
-        )
-    except requests.exceptions.RequestException as e:
-        await query.message.reply_text(
-            f"Не удалось получить гороскоп для {selected_sign}. Попробуйте позже.",
-            reply_markup=get_main_menu(),
-        )
-        print(f"Ошибка при запросе гороскопа: {e}")
-
-async def fetch_website_content(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    url = "https://horo.mail.ru"  # Укажите URL
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Проверка успешности запроса
-        content = response.text  # Если это HTML
-        # Если возвращается JSON:
-        # content = response.json()
-
-        await update.message.reply_text(f"Данные с сайта:\n{content}")
-    except requests.exceptions.RequestException as e:
-        await update.message.reply_text("Не удалось загрузить данные с сайта.")
-        print(f"Ошибка запроса: {e}")
 
 def main():
     bot_token = "7619104440:AAHdFpaYtoCS6pE3B9Ii8WJjU0KaR4YxVFA"
     app = Application.builder().token(bot_token).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("fetch_content", fetch_website_content))  # Новый обработчик
-    app.add_handler(CallbackQueryHandler(handle_horoscope_selection, pattern=r"^horoscope_"))
-    app.add_handler(CommandHandler("edit_facts", edit_facts))
     app.add_handler(CommandHandler("edit_calendar", edit_calendar))
+    app.add_handler(CommandHandler("edit_tasks", edit_tasks))
+    app.add_handler(CommandHandler("add_meme", add_meme))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(CommandHandler("calendar", calendar))
-    app.add_handler(CommandHandler("horoscope", horoscope))
 
     app.run_polling()
 
 if __name__ == "__main__":
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    # Регистрация команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запуск вебхука и переключение в режим pollings
+    application.run_polling()
     main()
 
